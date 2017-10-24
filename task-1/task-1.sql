a6e632b1dcfa355f87be5db0198d58f99f1ab898
Oleg Shabashov <oleg.shabashov@gmail.com>
2017-10-24 16:19:37 +0700

    Added foreign key for `comments`.`user_id`

--- task-1/task-1.sql
+++ task-1/task-1.sql
@@ -1,6 +1,9 @@
 /* Для увеличения быстродействия можно добавить индекс на `comments`.`user_id` */
 ALTER TABLE `comments` ADD INDEX (`user_id`);
 
+/* Для повышения консистентности можно добавить внешний ключ на `comments`.`user_id` */
+ALTER TABLE `comments` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /* Решение 1 задания */
 SELECT
   MAX(`comments`.`id`) `comment_id`, `users`.`name`, MAX(`text`) `text`, `user_id`
