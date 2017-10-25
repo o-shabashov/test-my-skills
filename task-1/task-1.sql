7d6435f1dc6463c981e74b0785c845690e180782
Oleg Shabashov <oleg.shabashov@gmail.com>
2017-10-25 15:20:32 +0700

    Added second solution for task 1

--- task-1/task-1.sql
+++ task-1/task-1.sql
@@ -4,9 +4,16 @@ ALTER TABLE `comments` ADD INDEX (`user_id`);
 /* Для повышения консистентности можно добавить внешний ключ на `comments`.`user_id` */
 ALTER TABLE `comments` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/* Решение 1 задания */
+/* Вариант 1, с подзапросом */
+SELECT `name`, `text`
+FROM `users` `u`
+  JOIN `comments` `c` ON `u`.`id` = `c`.`user_id`
+    AND `c`.`id` > (SELECT `id` FROM `comments` WHERE `user_id` = `u`.`id` ORDER BY `id` DESC LIMIT 1,1);
+
+/* Вариант 2, без подзапроса */
 SELECT
   MAX(`comments`.`id`) `comment_id`, `users`.`name`, MAX(`text`) `text`, `user_id`
-FROM `comments`
-  LEFT JOIN `users` ON `comments`.`user_id` = `users`.`id`
-GROUP BY `user_id`;
+FROM `users`
+  LEFT JOIN `comments` ON `users`.`id` = `comments`.`user_id`
+WHERE `comments`.`user_id` IS NOT NULL
+GROUP BY `comments`.`user_id`;
