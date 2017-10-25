7f43dffc2db2be3c408eb3a320fef84ebf810d08
Oleg Shabashov <oleg.shabashov@gmail.com>
2017-10-25 23:00:26 +0700

    Updated task-1 solution, LEFT join replaced by INNER

--- task-1/task-1.sql
+++ task-1/task-1.sql
@@ -7,13 +7,12 @@ ALTER TABLE `comments` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON 
 /* Вариант 1, с подзапросом */
 SELECT `name`, `text`
 FROM `users` `u`
-  JOIN `comments` `c` ON `u`.`id` = `c`.`user_id`
+  INNER JOIN `comments` `c` ON `u`.`id` = `c`.`user_id`
     AND `c`.`id` > (SELECT `id` FROM `comments` WHERE `user_id` = `u`.`id` ORDER BY `id` DESC LIMIT 1,1);
 
 /* Вариант 2, без подзапроса */
 SELECT
   MAX(`comments`.`id`) `comment_id`, `users`.`name`, MAX(`text`) `text`, `user_id`
 FROM `users`
-  LEFT JOIN `comments` ON `users`.`id` = `comments`.`user_id`
-WHERE `comments`.`user_id` IS NOT NULL
+  INNER JOIN `comments` ON `users`.`id` = `comments`.`user_id`
 GROUP BY `comments`.`user_id`;
